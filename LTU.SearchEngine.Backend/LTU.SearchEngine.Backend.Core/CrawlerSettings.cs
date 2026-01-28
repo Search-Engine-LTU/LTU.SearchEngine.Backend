7967d1e5326dd9cfa87f908d6a6b3f599946d9c6
a21jeaha <hisui86@gmail.com>
2026-01-28 14:04:51 +0100

    Create CrawlerSettings.cs
    not in a finished state yet.

---
+++ LTU.SearchEngine.Backend/LTU.SearchEngine.Backend.Core/CrawlerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LTU.SearchEngine.Backend.Core;
+
+public class CrawlerSettings
+{
+	[Required]
+	public string UserAgent { get; set; } 
+	public int MaxConcurrencyPerDomain { get; set; }
+	public int MinDelayMs { get; set; }
+
+
+}
